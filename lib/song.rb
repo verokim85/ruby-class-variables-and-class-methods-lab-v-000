58dfdccbd5d9c8545c3a3fc5f4d51560c16bfc17
Veronica Kim <vero.a.kim@gmail.com>
2018-02-15 00:19:38 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+
+  @@count = 0
+  @@artist = 0
+
+  def initialize(name, artist, genre)
+    @@count+=1
+    @name=name
+    @artist= artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def name=name
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+def artist=artist
+  @artist = artist
+end
+
+def artist
+  @artist
+end
+
+def genre= genre
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+
+
+
+
+
+end
